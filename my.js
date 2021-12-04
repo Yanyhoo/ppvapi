6abeea646540249a23759fcaae67dcd56dd0131d
Yanyhoo <76249479+Yanyhoo@users.noreply.github.com>
2021-12-04 17:46:24 +0100

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
 function testGS() {
 
-	const url = "https://script.google.com/macros/s/AKfycbwXue2kmkRJ0QIdrPkBsj3mmr_xXaL-IrvczyH4CPAu7PAt_aGBizB3Pg5HUzkhNHbPfg/exec"
+	const url = "https://script.google.com/macros/s/AKfycbz31vAWCc9mx95dfJjsIghRa4KyN9OwJG3dQvPDs130i6qr9wr5pgSNHp4hV0igIe2s/exec"
 
 	fetch(url)
 		.then(d => d.json())
