2f584d22b6388fccdcabf547f6905f0d4f9d01bf
Yanyhoo <jiri.janda@elinet.cz>
2021-12-07 21:00:46 +0100

    Upravené názvy

--- my.js
+++ my.js
@@ -28,7 +28,7 @@ function addGS(){
 		},
 		redirect: 'follow', // manual, *follow, error
 		//referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-		body: JSON.stringify({"Jméno":"Martin","Příjmení":"Urner","Klub":"Aeroklub Praha Letňany","Třída":"Klub","Typ letadla":"ASW15b","Imatrikulace":"OK-1985","Startovní znak":"JJ","Email":"UrnerM@seznam.cz","Mobil":"","Startovné":"ne"}) // body data type must match "Content-Type" header
+		body: JSON.stringify({"name":"Martin","surname":"Urner","aeroclub":"Aeroklub Praha Letňany","raceclass":"Klub","glider":"ASW15b","imatriculation":"OK-1985","code":"JJ","email":"UrnerM@seznam.cz","phone":"","Startovné":"ne"}) // body data type must match "Content-Type" header
 	  
 	});
 		
